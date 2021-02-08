3c860fe3566981040b8b5fc40e6d4f399e90a9ec
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-02-08 00:09:08 +0000

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,58 @@
+const pokeContainer = document.getElementById('poke-container');
+const pokemonCount = 150;
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#F4E7DA',
+  rock: '#D5D5D4',
+  fairy: '#FCEAFF',
+  poison: '#98D7A5',
+  bug: '#F8D5A3',
+  dragon: '#97B3E6',
+  psychic: '#EAEDA1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+};
+
+const mainTypes = Object.keys(colors);
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+  const res = await fetch(url);
+  const data = await res.json();
+  createPokemonCard(data);
+};
+
+fetchPokemons();
+
+const createPokemonCard = (pokemon) => {
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, '0');
+  const pokemonElement = document.createElement('div');
+  const pokemonTypes = pokemon.types.map((type) => type.type.name);
+  const type = mainTypes.find((type) => pokemonTypes.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonElement.classList.add('pokemon');
+  pokemonElement.style.backgroundColor = color;
+  pokemonElement.innerHTML = `
+    <div class="img-container">
+      <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="" />
+    </div>
+    <div class="info">
+      <span class="number">#${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span></small>
+    </div>`;
+
+  pokeContainer.appendChild(pokemonElement);
+};
